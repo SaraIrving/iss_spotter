670fb6163594b8113001b1def4f702b310a5e155
Sara Irving <sarakirving@gmail.com>
2020-07-03 04:32:41 +0000

    removed commented out code

--- index.js
+++ index.js
@@ -5,49 +5,13 @@ const { fetchISSFlyOverTimes } = require('./iss');
 const { nextISSTimesForMyLocation } = require('./iss');
 
 
-// fetchMyIP((error, ip) => {
-//   if (error) {
-//     console.log("It didn't work!" , error);
-//     return;
-//   }
-
-//   console.log('It worked! Returned IP:' , ip);
-// });
-
-// fetchCoordsByIp('172.103.226.92', (error, coords) => {
-//   if (error) {
-//     console.log("It didn't work!", error);
-//     return;
-//   }
-
-//   console.log("It worked! Returned Coords are:", coords);
-// });
-
-
-
-// const exampleCoords = { latitude: '49.27670', longitude: '-123.13000' };
-// fetchISSFlyOverTimes(exampleCoords, (error, passTimes) => {
-//   if (error) {
-//     console.log("It didn't work!", error);
-//     return;
-//   }
-  
-//   console.log("It worked! The returned flyover times are: ", passTimes);
-// })
-
-
 nextISSTimesForMyLocation((error, passTimes) => {
   if (error) {
     return console.log("It didn't work!", error);
   }
-  // success, print out the deets!
   makePassTimesLegible(passTimes);
 });
 
-//passTimes is an array of objects 
-//need to filter the time and date to a more readable form 
-//put this logic in a function so you can call it within the flow
-//first loop through the 5 responses 
 
 const makePassTimesLegible = function(passTimes) {
   for (const object of passTimes) {
