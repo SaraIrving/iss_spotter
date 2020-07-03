31d24ce685f4eda76b0ec4253ca53802a2f6eb74
Sara Irving <sarakirving@gmail.com>
2020-07-03 03:01:12 +0000

    called fetchISSFlyoverTimes function and provided sample input

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // index.js
 const { fetchMyIP } = require('./iss');
 const { fetchCoordsByIp } = require('./iss');
+const { fetchISSFlyOverTimes } = require('./iss');
 
 
 // fetchMyIP((error, ip) => {
@@ -12,11 +13,22 @@ const { fetchCoordsByIp } = require('./iss');
 //   console.log('It worked! Returned IP:' , ip);
 // });
 
-fetchCoordsByIp('7', (error, coords) => {
+// fetchCoordsByIp('172.103.226.92', (error, coords) => {
+//   if (error) {
+//     console.log("It didn't work!", error);
+//     return;
+//   }
+
+//   console.log("It worked! Returned Coords:", coords);
+// });
+
+const exampleCoords = { latitude: '49.27670', longitude: '-123.13000' };
+
+fetchISSFlyOverTimes(exampleCoords, (error, passTimes) => {
   if (error) {
     console.log("It didn't work!", error);
     return;
   }
-
-  console.log("It worked! Returned Coords:", coords);
-});
+  
+  console.log("It worked! The returned flyover times are: ", passTimes);
+})
