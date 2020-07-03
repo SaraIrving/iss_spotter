9c51757ddab7cc3ed1a4b81a8f189f790f1fb0f4
Sara Irving <sarakirving@gmail.com>
2020-07-03 02:32:03 +0000

    Defined fetchCoordsByIP function and exported it

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // index.js
 const { fetchMyIP } = require('./iss');
+const { fetchCoordsByIp } = require('./iss');
 
 
 // fetchMyIP((error, ip) => {
@@ -10,3 +11,12 @@ const { fetchMyIP } = require('./iss');
 
 //   console.log('It worked! Returned IP:' , ip);
 // });
+
+fetchCoordsByIp('7', (error, coords) => {
+  if (error) {
+    console.log("It didn't work!", error);
+    return;
+  }
+
+  console.log("It worked! Returned Coords:", coords);
+});
