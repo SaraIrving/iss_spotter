80433e1ae15be33e4e217b58169a2944fd805edf
Sara Irving <sarakirving@gmail.com>
2020-07-03 04:29:27 +0000

    logic for filtering the passTimes to make them readable

--- index.js
+++ index.js
@@ -41,5 +41,18 @@ nextISSTimesForMyLocation((error, passTimes) => {
     return console.log("It didn't work!", error);
   }
   // success, print out the deets!
-  console.log(passTimes);
+  makePassTimesLegible(passTimes);
 });
+
+//passTimes is an array of objects 
+//need to filter the time and date to a more readable form 
+//put this logic in a function so you can call it within the flow
+//first loop through the 5 responses 
+
+const makePassTimesLegible = function(passTimes) {
+  for (const object of passTimes) {
+    let time = object.duration;
+    let date = new Date(object.risetime * 1000)
+    console.log('Next pass at ' + date.toString() + ' for ' + time + ' seconds.')
+  }
+}
