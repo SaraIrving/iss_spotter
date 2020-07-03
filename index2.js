ea084d4bb5461e8405586038312653fd5e11224d
Sara Irving <sarakirving@gmail.com>
2020-07-03 05:33:43 +0000

    Copied in makePassTimesLegible function from index.js

--- index2.js
+++ index2.js
@@ -3,7 +3,20 @@ const { fetchCoordsByIp } = require('./iss_promised');
 const { fetchISSFlyOverTimes } = require('./iss_promised');
 const { nextISSTimesForMyLocation } = require('./iss_promised');
 
-fetchMyIP()
-  .then(fetchCoordsByIp)
-  .then(body => console.log(body));
+// fetchMyIP()
+//   .then(fetchCoordsByIp)
+//   .then(fetchISSFlyOverTimes)
+//   .then(body => console.log(body))
   
+const makePassTimesLegible = function(passTimes) {
+  for (const object of passTimes) {
+    let time = object.duration;
+    let date = new Date(object.risetime * 1000)
+    console.log('Next pass at ' + date.toString() + ' for ' + time + ' seconds.')
+  }
+}
+
+nextISSTimesForMyLocation()
+  .then((flyoverTimes) => {
+    makePassTimesLegible(flyoverTimes);
+  })
